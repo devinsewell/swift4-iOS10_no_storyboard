cf973b93164124c03cac36b573b171460a2a3b75
Devin Sewell <devinsewell@gmail.com>
2018-10-30 11:08:05 -0700

    v1.0
    How to make Swift4 iOS10 app without Storyboard. Bonus: LaunchScreen

---
+++ Blank App/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Blank App
+//
+//  Created by DBOI on 10/29/18.
+//  Copyright © 2018 ios100. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
